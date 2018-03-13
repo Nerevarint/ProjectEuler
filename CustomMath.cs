a2f104541ec303fbfa395f6a942d4d1e2e2b1494
Kirill <Arkrait04@gmail.com>
2018-03-13 21:59:55 +0300

    added solved problems' methods

---
+++ CustomMath.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace Tutorials
+ {
+     public static class CustomMath
+     {
+         /*public static string TruncateString(string x, int length)
+         {
+             return x.Substring(0, Math.Min(x.Length, length));
+         }*/
+
+         public static bool IsPalindrome(int n)
+         {
+             string str = n.ToString();
+             int digitCount = str.Length;
+             string firstPart = str.Substring(0, digitCount / 2);
+             string secondPart = str.Substring(digitCount / 2, digitCount / 2);
+             string secondPartReversed1 = Reverse(secondPart);
+
+             if (firstPart == secondPartReversed1)
+                 return true;
+             return false;
+         }
+         public static string Reverse(string s)
+         {
+             char[] charArray = s.ToCharArray();
+             Array.Reverse( charArray );
+             return new string( charArray );
+         }
+         public static bool IsDivisibleForEachN(int x, int n) /*Checks if number can be divided by every 
+                                                                 natural number until n without a remainder*/ 
+         {
+             int y = 0;
+             for (int i = 1; i <= n; i++)
+             {
+                 if (x % i == 0)
+                 {
+                     y++;
+                 }
+             }
+
+             if (y == n)
+             {
+                 return true;
+             }
+
+             return false;
+         }
+         public static int Sum(int n) //Sum of first n numbers
+         {
+             int sum = 0;
+             for (int i = 1; i < n; i++)
+             {
+                 sum += i;
+             }
+
+             return sum;
+         }
+         public static List<int> Factors(long n)
+         {
+             int x = (int)Math.Floor(Math.Sqrt(n));
+             List<int> list = new List<int>();
+             for (int i = 1; i < x; i++)
+             {
+                 if (n % i == 0)
+                 {
+                     list.Add(i);            /*Adds a factor and a number divided 
+                                                by that factor to list*/
+                     list.Add((int)n / i);
+                 }
+             }
+
+             return list;
+         }
+
+         public static bool IsPrime(long n)
+         {
+             int x = (int) Math.Floor(Math.Sqrt(n));
+
+             if (n == 1)
+             {
+                 return false;
+             }
+
+             if (n == 2)
+             {
+                 return true;
+             }
+
+             for (int i = 2; i <= x; ++i)
+             {
+                 if (n % i == 0)        /*checks if number can be divided by any 
+                                             number until the sqrt of that number*/
+                 {
+                     return false;
+                 }
+             }
+
+             return true;
+         }
+     }
+ }
---
+++ Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Tutorials
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var watch = Stopwatch.StartNew();
+            //Call Problem{problem number} method here for result
+            watch.Stop();
+            Console.WriteLine(watch.Elapsed);
+        }
+
+        public static void Problem1()
+        {
+            int sum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+
+        }
+
+        public static void Problem2()
+        {
+            long f1 = 1;
+            long f2 = 1;
+            long result = 0;
+            long sum = 0;
+
+            while (result < 4_000_000)
+            {
+                if (result % 2 == 0)
+                {
+                    sum += result;
+                }
+
+                result = f1 + f2;
+                f2 = f1;
+                f1 = result;
+            }
+            Console.WriteLine(sum);
+        }
+
+        public static void Problem3()
+        {
+            const long NUMBER = 600851475143;
+            int max = 0;
+            foreach (var c in CustomMath.Factors(NUMBER)) //CustomMath methods are in another file
+            {
+                if (CustomMath.IsPrime(c))
+                {
+                    if (c > max)
+                    {
+                        max = c; 
+                    }
+                }
+            }
+            Console.WriteLine(max);
+        }
+
+        public static void Problem4()
+        {
+            int largestPalindrome = 0;
+            for (int i = 999; i > 99; i--)
+            {
+                for (int j = 999; j > 99; j--)
+                {
+                    if (CustomMath.IsPalindrome(i * j))
+                    {
+                        if (i * j >largestPalindrome)
+                        {
+                            largestPalindrome = i * j;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine(largestPalindrome);
+        }
+
+        public static void Problem5()
+        {
+            int n = 1;
+            while (true)
+            {
+                if (CustomMath.IsDivisibleForEachN(n, 20))
+                {
+                    break;
+                }
+
+                n++;
+            }
+            Console.WriteLine(n); //returns smallest number which can be divided by every natural number up to 20
+        }
+
+        public static void Problem6()
+        {
+            const int N = 100;
+            int sumsqr = 0, sqrsum = 0;
+            for (int i = 1; i <= N; i++)
+            {
+                sqrsum += i;
+                sumsqr += i * i;
+            }
+            Console.WriteLine(sqrsum * sqrsum - sumsqr);
+        }
+
+        public static void Problem7()
+        {
+            int n = 2;
+            const int N = 10001;
+            int ctr = 0;
+            while (ctr < N)
+            {
+                if (CustomMath.IsPrime(n))
+                {
+                    ctr++;
+                }
+
+                n++;
+            }
+
+            n--;
+            Console.WriteLine(n);
+        }
+
+        public static void Problem10()        /*The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+                                                Find the sum of all the primes below two million.*/
+        {
+            int n = 2;
+            long sum = 0;
+            while (n < 2_000_000)
+            {
+                if (CustomMath.IsPrime(n))
+                {
+                    sum += n;
+                }
+
+                n++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
